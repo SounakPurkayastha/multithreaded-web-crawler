1cf44fd08245f51ed133a61583c913eeff0d3875
Sounak <sounak.purkayastha@gmail.com>
2026-03-14 16:55:20 +0530

    Jsoup HTML parsing and crawling logic added

--- src/main/java/webcrawler/CrawlTask.java
+++ src/main/java/webcrawler/CrawlTask.java
@@ -1,8 +1,19 @@
 package webcrawler;
 
+import java.io.IOException;
+import java.net.URI;
+import java.net.http.HttpClient;
+import java.net.http.HttpRequest;
+import java.net.http.HttpResponse;
+import java.time.Duration;
 import java.util.Set;
 import java.util.concurrent.LinkedBlockingQueue;
 import java.util.concurrent.atomic.AtomicInteger;
+
+import org.jsoup.Jsoup;
+import org.jsoup.nodes.Document;
+import org.jsoup.nodes.Element;
+import org.jsoup.select.Elements;
 
 public class CrawlTask implements Runnable {
 
@@ -10,25 +21,52 @@ public class CrawlTask implements Runnable {
     private final Set<String> visited;
     private final LinkedBlockingQueue<String> queue;
     private final AtomicInteger pagesCrawled;
+    private final HttpClient client;
 
-    public CrawlTask(String url, Set<String> visited, LinkedBlockingQueue<String> queue, AtomicInteger pagesCrawled) {
+    public CrawlTask(String url, Set<String> visited, LinkedBlockingQueue<String> queue, AtomicInteger pagesCrawled, HttpClient client) {
         this.url = url;
         this.visited = visited;
         this.queue = queue;
         this.pagesCrawled = pagesCrawled;
+        this.client = client;
     }
 
     public void run() {
-        if(visited.add(url)) {
-            queue.add(url + "/link-" + pagesCrawled.get());
-            pagesCrawled.incrementAndGet();
-            System.out.println(Thread.currentThread().getName() + " is crawling " + url);
+        if(pagesCrawled.get() >= 10)
+            return;
+        if(visited.add(url)) {  // adds element to set if not already present and returns true else false
             try {
-                Thread.sleep(500);
-            } catch(InterruptedException e) {
+                HttpRequest request = HttpRequest.newBuilder()
+                                        .uri(URI.create(url))
+                                        .header("User-Agent", "Mozilla/5.0 (Macintosh; Intel Mac OS X 10_15_7) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/119.0.0.0 Safari/537.36") // request rejected without proper User-Agent 
+                                        .timeout(Duration.ofSeconds(10)) // read timeout, different from connect timeout
+                                        .build();
+
+                HttpResponse<String> response = client.send(request, HttpResponse.BodyHandlers.ofString());
+
+                if(response.statusCode() == 200) {
+                    Document doc = Jsoup.parse(response.body());
+                    Elements links = doc.select("a[href]");
+
+                    for(Element link : links) {
+                        String discoveredUrl = link.attr("abs:href");
+                        if(!discoveredUrl.isBlank() && pagesCrawled.get() < 10) {
+                            // queue.add(discoveredUrl);
+                            queue.put(discoveredUrl);   // blocks if queue is bounded and full
+                            // System.out.println(discoveredUrl + " added to queue.");
+                        }
+                    }
+
+                    Integer count = pagesCrawled.incrementAndGet();
+                    System.out.println("Crawled " + url + " " + count);
+                }
+                else {
+                    System.out.println("Skipped " + url + " Status Code:" + response.statusCode());
+                }
+
+            } catch(Exception e) {
                 e.printStackTrace();
-            }
-            System.out.println(Thread.currentThread().getName() + " completed crawling " + url);
+            } 
         }
     }
 
--- src/main/java/webcrawler/Main.java
+++ src/main/java/webcrawler/Main.java
@@ -1,5 +1,7 @@
 package webcrawler;
 
+import java.net.http.HttpClient;
+import java.time.Duration;
 import java.util.Set;
 import java.util.concurrent.ConcurrentHashMap;
 import java.util.concurrent.ExecutorService;
@@ -17,15 +19,24 @@ public class Main {
         ExecutorService executorService = Executors.newFixedThreadPool(3);
         Set<String> visited = ConcurrentHashMap.newKeySet();
 
+        // HttpClient.Redirect.NEVER    -->   (Default) Does not follow redirects. You must handle them manually.
+        // HttpClient.Redirect.ALWAYS   -->   Always follows redirects, even from HTTPS to HTTP (less secure).
+        // HttpClient.Redirect.NORMAL   -->   Follows redirects unless they go from HTTPS to HTTP.
+        HttpClient client = HttpClient.newBuilder()
+                                .followRedirects(HttpClient.Redirect.NORMAL)
+                                .connectTimeout(Duration.ofSeconds(5))
+                                .build();
 
-        queue.add("www.metal-archives.net");
 
-        while(pagesCrawled.get() < 10) {
+        queue.add("https://en.wikipedia.org/wiki/Java_(programming_language)");
+
+        while(pagesCrawled.get() < 10) {    
+            // System.out.println(pagesCrawled);    // only crawl 10 pages
             try{
                 // String url = queue.take();   blocking call, waits forever if queue is empty
                 String url = queue.poll(5, TimeUnit.SECONDS);
                 if(url != null)
-                    executorService.execute(new CrawlTask(url, visited, queue, pagesCrawled));
+                    executorService.execute(new CrawlTask(url, visited, queue, pagesCrawled, client));  // submits task to be executed
             } catch(InterruptedException e) {
                 e.printStackTrace();
             }
@@ -34,7 +45,9 @@ public class Main {
         executorService.shutdown(); // Stop accepting new tasks, but let the workers finish the ones currently in the queue
        
         try {
-            executorService.awaitTermination(10, TimeUnit.SECONDS); // I will wait here for up to 10 seconds for the workers to finish. If they aren't done by then, I'm moving on.
+            if(!executorService.awaitTermination(60, TimeUnit.SECONDS)) {  // I will wait here for up to 60 seconds for the workers
+                executorService.shutdownNow();                             //  to finish. If they aren't done by then, I'm moving on.
+            }                                                                         
         } catch(InterruptedException e) {
             e.printStackTrace();
         }
