2fe54fa9b4af3d2ae80c0341ca391a8fbeeaf6f3
Sounak <sounak.purkayastha@gmail.com>
2026-03-28 19:38:47 +0530

    Added 404 and timeout error tests

--- src/test/java/webcrawler/CrawlTaskTest.java
+++ src/test/java/webcrawler/CrawlTaskTest.java
@@ -1,6 +1,7 @@
 package webcrawler;
 
 import static org.junit.jupiter.api.Assertions.assertEquals;
+import static org.junit.jupiter.api.Assertions.assertTrue;
 import static org.mockito.ArgumentMatchers.any;
 import static org.mockito.ArgumentMatchers.anyString;
 import static org.mockito.ArgumentMatchers.eq;
@@ -62,5 +63,41 @@ public class CrawlTaskTest {
             verify(mockJedis).sadd(anyString(), eq(url));   // It ensures that your application actually attempted to call the sadd command
                                                             // By using eq(url), the test ensures that the exact value stored in your url variable was the one sent to Redis
                                                             // Checks the Process (Did the code actually try to save to Redis?).
+    }
+
+    @Test
+    void shouldHandle404ErrorWithoutIncrementCounter() throws IOException, InterruptedException {
+
+        String url = "https://www.test.com";
+
+        when(mockJedis.sadd(anyString(), eq(url))).thenReturn(1L);
+        when(mockClient.send(any(), any(HttpResponse.BodyHandler.class))).thenReturn(mockResponse);
+        when(mockResponse.statusCode()).thenReturn(404);
+        
+
+        CrawlTask task = new CrawlTask(url, mockJedis, queue, pagesCrawled, 10, mockClient, totalBytes, totalTime, "test-key");
+
+        task.run();
+
+        assertEquals(0, pagesCrawled.get());
+        assertEquals(0, queue.size());
+        verify(mockJedis).sadd(anyString(), eq(url));
+    }
+
+    @Test
+    void shouldHandleNetworkTimeoutGracefully() throws Exception {
+
+        String url = "https://www.test.com";
+
+        when(mockJedis.sadd(anyString(), eq(url))).thenReturn(1L);
+        when(mockClient.send(any(), any(HttpResponse.BodyHandler.class))).thenThrow(new IOException("Connection Timeout"));
+
+        CrawlTask task = new CrawlTask(url, mockJedis, queue, pagesCrawled, 10, mockClient, totalBytes, totalTime, "test-key");
+        
+        task.run();
+
+        assertEquals(0, pagesCrawled.get());
+        assertTrue(queue.isEmpty());
+        verify(mockJedis).sadd(anyString(), eq(url));
     }
 }
